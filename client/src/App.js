a7167e49d3ad89bfc471323a4a37323aebdb69d5
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2021-08-30 21:12:50 +0530

    Added header backdrop and custom scrollbars

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import Scrollbars from './components/Scrollbars';
+
 import Background from './components/Background';
 import Routes from './routes';
 import './App.css';
@@ -7,10 +9,12 @@ import './App.css';
 function App() {
 	return (
 		<div>
-			<Background/>
-			<Router>
-				<Routes/>
-			</Router>
+			<Scrollbars autoHide autoHideTimeout={2000} style={{ height: '100vh', width: '100vw' }}>
+				<Background/>
+				<Router>
+					<Routes/>
+				</Router>
+			</Scrollbars>
 		</div>
 	);
 }
---
+++ client/src/components/Scrollbars.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+export default class ColoredScrollbars extends Component {
+
+    constructor(props, ...rest) {
+        super(props, ...rest);
+        this.state = { top: 0 };
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.renderThumb = this.renderThumb.bind(this);
+        this.renderTrackHorizontal = this.renderTrackHorizontal.bind(this);
+        this.renderTrackVertical = this.renderTrackVertical.bind(this);
+    }
+
+    handleUpdate(values) {
+        const { top } = values;
+        this.setState({ top });
+    }
+
+    renderThumb({ style, ...props }) {
+        const thumbStyle = {
+            backgroundColor: "#ffffff",
+            borderRadius: "60px"
+        };
+        return (
+            <div
+                style={{ ...style, ...thumbStyle }}
+                {...props}
+            />
+        );
+    }
+
+    renderTrackHorizontal({ style, ...props }) {
+        const trackStyle = {
+            backgroundColor: "#0E141B",
+            zIndex: "9999",
+            height: "8px",
+            width: "100vh",
+            bottom: "0"
+        };
+        return (
+            <div
+                style={{ ...style, ...trackStyle }}
+                {...props}
+            />
+        )
+    }
+
+    renderTrackVertical({ style, ...props }) {
+        const trackStyle = {
+            backgroundColor: "#0E141B",
+            zIndex: "9999",
+            height: "100vh",
+            width: "8px",
+            right: "0"
+        };
+        return (
+            <div
+                style={{ ...style, ...trackStyle }}
+                {...props}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <Scrollbars
+                renderThumbHorizontal={this.renderThumb}
+                renderThumbVertical={this.renderThumb}
+                renderTrackHorizontal={this.renderTrackHorizontal}
+                renderTrackVertical={this.renderTrackVertical}
+                onUpdate={this.handleUpdate}
+                {...this.props}/>
+        );
+    }
+}
