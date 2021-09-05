50c5b217ca91bbfceb829b1c254f5363c77d0454
Om Patil <200010036@iitdh.ac.in>
2021-09-05 16:44:04 +0530

    Pulling event data from json now

--- client/src/components/Event/index.jsx
+++ client/src/components/Event/index.jsx
@@ -6,16 +6,16 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-const EventComp = ({parity}) => {
-	const containerClass = parity ? "flex-row-reverse" : "";
+const EventComp = ({event, index}) => {
+	const containerClass = index%2 ? "flex-row-reverse" : "";
 	return (
 		<Container fluid className="py-5">
 		  <Row className={"justify-content-center text-white align-items-center " + containerClass}>
 		    <Col lg="5" className="px-5 px-lg-0">
-		    	<Carousel />
+		    	<Carousel imgs={event.imgs} />
 		    </Col>
 		    <Col lg="5" className="px-5 text-center body">
-		    	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		    	{event.desc}
 		    </Col>
 		  </Row>
 		</Container>
--- client/src/pages/Events/index.jsx
+++ client/src/pages/Events/index.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-
 import EventComp from "../../components/Event";
 import Header from '../../components/Header';
 import './styles.css';
+import EventData from '../../shared/events.json'
 
 const Events = () => {
     return (
         <div>
             <Header active={2}/>
             <div className="content">
-                <Fade right delay={100}>
-                    <EventComp parity={0}/>
-                </Fade>
-                <Fade left delay={100}>
-                    <EventComp parity={1}/>
-                </Fade>
-                <Fade right delay={100}>
-                    <EventComp parity={0}/>
-                </Fade>
+                {EventData.map((data, index) => (
+                    <Fade right delay={100}>
+                        <EventComp event={data} index={index}/>
+                    </Fade>
+                ))}
             </div>
         </div>
     )
