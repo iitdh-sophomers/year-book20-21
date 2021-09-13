03dbbe6d36edc076223a2f6da3d7bc45b9071d1e
Om Patil <200010036@iitdh.ac.in>
2021-09-13 23:16:08 +0530

    Update PDF link

--- client/src/shared/pdfURL.js
+++ client/src/shared/pdfURL.js
@@ -1,1 +1,1 @@
-export const url = "https://drive.google.com/file/d/17O_RKSLE_X6IA93JMyAVVsO1MJXtYDZ3/view?usp=sharing";
+export const url = "https://drive.google.com/file/d/1tXf12Jdaqg169m3PSEY4CZi-tWcwC_Z2/view?usp=sharing";
