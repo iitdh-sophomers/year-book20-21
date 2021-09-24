4c5dcfa8e01e3147327bdf3912e89dfa77accdfe
Samy <200020040@iitdh.ac.in>
2021-09-24 21:01:13 +0530

    Added Demo Image

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <Slide>
             <div className="footer background-black">
-                <h6 className="text white">If you want to contribute, do give a visit to our <a href="https://github.com/iitdh-sophomers/year-book20-21" rel="noreferrer" target="_blank" className="a">Github Page</a> &nbsp; || &nbsp; And read the contribution guide <a href="https://github.com/iitdh-sophomers/year-book20-21/blob/master/CONTRIBUTION_GUIDE.MD" rel="noreferrer" target="_blank" className="a">here</a></h6>
+                <h6 className="text white">If you want to contribute, do give a visit to our <a href="https://github.com/iitdh-sophomers/year-book20-21" rel="noreferrer" target="_blank" className="a">Github Page</a> and read the contribution guide <a href="https://github.com/iitdh-sophomers/year-book20-21/blob/master/CONTRIBUTION_GUIDE.MD" rel="noreferrer" target="_blank" className="a">here</a></h6>
             </div>
         </Slide>
     )
